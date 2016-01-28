7ca5cab6fce5e804369c9c73da8232e96b305477
aliz <aliz@011-11.bxsci.78x445.nycboe.org>
2016-01-28 15:58:23 -0500

    Create PID subsystem class.

---
+++ src/org/usfirst/frc/team2265/robot/subsystems/PID.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team2265.robot.subsystems;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.PIDCommand;
+
+public class PID extends Subsystem {
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+	}
+	public double desiredVelocity = 0.475;
+		 
+		
+		
+}
