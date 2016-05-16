e32c9b4bf6370d9a0c1abe91e7262fc6e1a66780
kat-wicks <wicksk@bxscience.edu>
2016-05-16 15:43:36 -0400

    Update Shoot.java
    Adds comments

--- src/org/usfirst/frc/team2265/robot/commands/Shoot.java
+++ src/org/usfirst/frc/team2265/robot/commands/Shoot.java
@@ -1,3 +1,4 @@
+//This command shoots the ball.
 package org.usfirst.frc.team2265.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -8,27 +9,18 @@ import org.usfirst.frc.team2265.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class Shoot extends Command {
 	double v;
-	//boolean done;
+	
     public Shoot(double velocity) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
     	v = velocity;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
-    	//turn off compressor
     	RobotMap.turningCam = false; 
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	//Robot.compressy.stop(); 
     	if (Robot.cannon.isHigh) {
     		Robot.cannon.spinWheels(v, v);
     		System.out.println(v + "");
@@ -44,23 +36,18 @@ public class Shoot extends Command {
         		RobotMap.turningCam = false;
         	}
     	}
-    	RobotMap.turningCam = false; 
-    	//done = true;
+    	RobotMap.turningCam = false;
     }
-
-    // Make this return true when this Command no longer needs to run execute()
+    
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	//Robot.compressy.start();
     	RobotMap.turningCam = false; 
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
