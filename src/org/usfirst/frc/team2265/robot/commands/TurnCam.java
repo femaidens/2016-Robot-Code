36463f05393cad618104e0b5f4463dee92d0f018
kat-wicks <wicksk@bxscience.edu>
2016-05-16 15:50:05 -0400

    Update TurnCam.java
    Adds comments

--- src/org/usfirst/frc/team2265/robot/commands/TurnCam.java
+++ src/org/usfirst/frc/team2265/robot/commands/TurnCam.java
@@ -1,3 +1,4 @@
+//This command changes the turnCam boolean to turn the cam.
 package org.usfirst.frc.team2265.robot.commands;
 
 import org.usfirst.frc.team2265.robot.Robot;
@@ -6,41 +7,21 @@ import org.usfirst.frc.team2265.robot.RobotMap;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class TurnCam extends Command {
 
-    public TurnCam() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	//requires(Robot.cannon); 
-    }
+    public TurnCam() {}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+    protected void initialize() {}
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	/*Robot.cannon.turnCam();
-    	Robot.cannon.camTalon.set(0.0);*/
     	RobotMap.turningCam = true;
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return true;
     }
+    
+    protected void end() {}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	//Robot.cannon.camTalon.set(0.0);
-    	//Robot.cannon.stop();
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+    protected void interrupted() {}
 }
